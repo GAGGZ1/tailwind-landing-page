840612fd43ed8238afed2b88a28c53b2e5866e86
Gagan Chauhan <81284966+GAGGZ1@users.noreply.github.com>
2025-05-20 19:53:17 +0530

    added Hover feature

--- script.js
+++ script.js
@@ -1,64 +1,57 @@
-const navDialog=document.getElementById('nav-dialog');
-function handleMenu(){
-  navDialog.classList.toggle('hidden');
+const navDialog = document.getElementById("nav-dialog");
+function handleMenu() {
+  navDialog.classList.toggle("hidden");
 }
 
-const initialTranslateLTR=-48*4;
-const initialTranslateRTL=36*4;
+const initialTranslateLTR = -48 * 4;
+const initialTranslateRTL = 36 * 4;
 
-function setupIntersectionObserver(element,isLTR,speed){
-  const intersectionCallback = (entries) =>{
-    const isIntersecting=entries[0].isIntersecting;
+function setupIntersectionObserver(element, isLTR, speed) {
+  const intersectionCallback = (entries) => {
+    const isIntersecting = entries[0].isIntersecting;
     // console.log(element,isIntersecting);
-    if(isIntersecting){
-      document.addEventListener('scroll',scrollHandler);
+    if (isIntersecting) {
+      document.addEventListener("scroll", scrollHandler);
+    } else {
+      document.removeEventListener("scroll", scrollHandler);
     }
-    else{
-      document.removeEventListener('scroll',scrollHandler);
-    }
-
-
-  }
-  const intersectionObserver=new IntersectionObserver(intersectionCallback)
+  };
+  const intersectionObserver = new IntersectionObserver(intersectionCallback);
 
   intersectionObserver.observe(element);
 
-  function scrollHandler(){
-    const translateX=(window.innerHeight - element.getBoundingClientRect().top)*speed;
+  function scrollHandler() {
+    const translateX =
+      (window.innerHeight - element.getBoundingClientRect().top) * speed;
 
-    let totalTranslate=0;
-    if(isLTR){
-      totalTranslate=translateX+initialTranslateLTR;
-
+    let totalTranslate = 0;
+    if (isLTR) {
+      totalTranslate = translateX + initialTranslateLTR;
+    } else {
+      totalTranslate = -(translateX + initialTranslateRTL);
     }
-    else{
-      totalTranslate=-(translateX+initialTranslateRTL);
-    }
-    element.style.transform=`translateX(${totalTranslate}px)`;
+    element.style.transform = `translateX(${totalTranslate}px)`;
   }
 }
 
+const line1 = document.getElementById("line1");
+const line2 = document.getElementById("line2");
+const line3 = document.getElementById("line3");
+const line4 = document.getElementById("line4");
 
-const line1=document.getElementById('line1')
-const line2=document.getElementById('line2')
-const line3=document.getElementById('line3')
-const line4=document.getElementById('line4')
+setupIntersectionObserver(line1, true, 0.15);
+setupIntersectionObserver(line2, false, 0.15);
+setupIntersectionObserver(line3, true, 0.15);
+setupIntersectionObserver(line4, true, 0.8);
 
-setupIntersectionObserver(line1,true,0.15)
-setupIntersectionObserver(line2,false,0.15)
-setupIntersectionObserver(line3,true,0.15)
-setupIntersectionObserver(line4,true,0.8)
+const dtElements = document.querySelectorAll("dt");
+dtElements.forEach((element) => {
+  element.addEventListener("click", () => {
+    const ddId = element.getAttribute("aria-controls");
+    const ddElement = document.getElementById(ddId);
+    const ddArrowIcon = element.querySelectorAll("i")[0];
 
-
-
-const dtElements=document.querySelectorAll('dt');
-dtElements.forEach(element => {
-  element.addEventListener('click',() =>{
-    const ddId=element.getAttribute('aria-controls');
-    const ddElement=document.getElementById(ddId);
-    const ddArrowIcon =element.querySelectorAll('i')[0];
-
-    ddElement.classList.toggle('hidden');
-    ddArrowIcon.classList.toggle('-rotate-180');
-  })
-})
+    ddElement.classList.toggle("hidden");
+    ddArrowIcon.classList.toggle("-rotate-180");
+  });
+});
